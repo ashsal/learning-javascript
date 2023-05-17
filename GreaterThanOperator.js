93257bc0fd048e63ce5c7be6994ae0f225f68f8a
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-17 09:21:22 +0200

    Greater than operator

---
+++ GreaterThanOperator.js
@@ -0,0 +1,16 @@
+let x = 5;
+let y = 4;
+if (x > y) {
+  console.log("x is greater than y");
+} else {
+  console.log("x is not greater than y");
+}
+
+console.log(x > y);
+
+if (x >= y) {
+  console.log("x is greater than or equal to y");
+} else {
+  console.log("x is not greater than or equal to y");
+}
+console.log(x >= y);
