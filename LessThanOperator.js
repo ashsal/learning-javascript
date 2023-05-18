b56675d2e2ce45ca08b7713fd66224d533d27c3c
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-18 11:07:49 +0200

    Less than operator example

---
+++ LessThanOperator.js
@@ -0,0 +1,18 @@
+let x = 5;
+let y = 4;
+
+if (x < y) {
+  console.log("x is less than y");
+} else {
+  console.log("x is not less than y");
+}
+
+console.log("x < y", x < y);
+
+if (x <= y) {
+  console.log("x is less than or equal to y");
+} else {
+  console.log("x is not less than or equal to y");
+}
+
+console.log("x <= y", x <= y);
