ea5e7b2c66aeff27c03801dc01d9930212334226
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-19 16:04:59 +0200

    Ternary operator examples

---
+++ TernaryOperator.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 6;
+// let result;
+// if (x == y) {
+//   result = x;
+// } else {
+//   result = y;
+// }
+
+result = x == y ? x : y;
+console.log("value of result is", result);
+
+x == y ? console.log("x is equal to y") : console.log("x is NOT equal to y");
