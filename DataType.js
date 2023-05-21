51917b55c86c482b3d289a7408d598bfa60a697a
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-21 20:56:12 +0200

    Data type examples

---
+++ DataType.js
@@ -0,0 +1,28 @@
+let x = "Ashish";
+console.log(x);
+
+let y = -2.6;
+console.log(y);
+
+let z = 9999999999999999n;
+console.log(z);
+
+let a = false;
+console.log(a);
+
+let b;
+console.log(b);
+
+let c = null;
+console.log(c);
+
+let d = { name: "Ashish", country: "India" };
+console.log(d);
+console.log(d.country);
+
+let e = ["Ashish", "Ram", "Shyam", "Ghanshyam"];
+console.log(e);
+console.log(e[1]);
+
+let today = new Date();
+console.log(today);
