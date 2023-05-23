a42c8c023c7bfbf5069f699e08f0bf3182716a91
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-23 09:59:36 +0200

    To fixed and math round examples

---
+++ DataTypes/ToFixedMathRoundExamples.js
@@ -0,0 +1,21 @@
+console.log(5 + 6);
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+console.log(sum(5, 6));
+console.log(sum(8, 5));
+
+const valueOfItem = 13.77;
+const valueOfItemAfterDiscount = valueOfItem / 2;
+console.log(valueOfItemAfterDiscount);
+console.log(valueOfItemAfterDiscount.toFixed(2));
+
+const x = 6.887;
+console.log(x.toFixed(2));
+
+const y = 12.2;
+console.log(y.toFixed(3));
+
+const z = 1200.4;
+console.log(Math.round(z));
