9873c68b8df7bbf3df942d62eafb066efe524d81
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-26 10:37:52 +0200

    Number constant examples

---
+++ DataTypes/NumberConstant.js
@@ -0,0 +1,18 @@
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log("INFINITY EXAMPLE 1", 10 / 0);
+console.log("Negative INFINITY EXAMPLE 1", -10 / 0);
+console.log("Multiplication INFINITY EXAMPLE 1", -2 * Infinity);
+
+const a = Number.MAX_VALUE;
+console.log("Number.MAX_VALUE is", Number.MAX_VALUE);
+console.log("Number.MAX_VALUE * 2", Number.MAX_VALUE * 2);
+
+console.log("MAX_SAFE_INTEGER is ", Number.MAX_SAFE_INTEGER);
+console.log("MAX_SAFE_INTEGER plus 1 is ", Number.MAX_SAFE_INTEGER + 1);
+console.log("MAX_SAFE_INTEGER plus 2 is ", Number.MAX_SAFE_INTEGER + 2);
+
+console.log("MIN_SAFE_INTEGER is", Number.MIN_SAFE_INTEGER);
