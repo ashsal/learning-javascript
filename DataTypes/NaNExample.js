740c9a0fb262dc56fe86706711787889a6bfe3fd
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-28 16:57:56 +0200

    NaN example

---
+++ DataTypes/NaNExample.js
@@ -0,0 +1,11 @@
+const result = "asdiqwioqwo" / 10;
+console.log(result);
+
+const result2 = parseInt("adasdadasd");
+console.log(result2);
+
+if (isNaN(result2)) {
+  console.log("Not a number found", result2);
+} else {
+  console.log("Number found", result2);
+}
