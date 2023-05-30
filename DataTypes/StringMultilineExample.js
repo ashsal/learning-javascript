56e7ad27e72534a1a6de8d678221d3a2ed545a46
Ashish Saluja <ashishsaluja1@gmail.com>
2023-05-30 18:55:51 +0200

    String multi line example

---
+++ DataTypes/StringMultilineExample.js
@@ -0,0 +1,14 @@
+const multiLineUsingBacktick = `First line
+Second line
+Third line`;
+console.log('multiLineUsingBacktick', multiLineUsingBacktick);
+
+const multilineUsingStringConatenation = "First line \n" +
+"Second line \n" +
+"Third line";
+console.log('multilineUsingStringConatenation', multilineUsingStringConatenation);
+
+const multilineUsingBackslash = "First line \n\
+Second line \n\
+Third line";
+console.log('multilineUsingBackslash', multilineUsingBackslash);
