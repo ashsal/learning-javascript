4e7e28e879c8927950a3db355da416ceeed4a590
Ashish Saluja <ashishsaluja1@gmail.com>
2023-06-01 10:02:56 +0200

    String length and case conversion examples

---
+++ DataTypes/StringLengthAndCaseExample.js
@@ -0,0 +1,14 @@
+const myName = "Ashish";
+console.log('First character', myName[0]);
+console.log('Second character', myName[1]);
+console.log('Second character', myName[5]);
+
+console.log('Length is', myName.length);
+
+const upperCaseMyName = myName.toUpperCase();
+console.log('Upper case my name is', upperCaseMyName);
+
+const lowerCaseMyName = myName.toLowerCase();
+console.log('Lower case my name is', lowerCaseMyName);
+
+console.log('myName is', myName);
