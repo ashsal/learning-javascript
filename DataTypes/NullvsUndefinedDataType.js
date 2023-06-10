ce9b0b9290c4fd30f0ba71e561f0b80406d97b8f
Ashish Saluja <ashishsaluja1@gmail.com>
2023-06-10 13:27:47 +0200

    Null vs undefined examples

---
+++ DataTypes/NullvsUndefinedDataType.js
@@ -0,0 +1,20 @@
+let a;
+console.log('a is', a);
+
+let b = null;
+console.log('b is', b);
+
+console.log(typeof 1);
+console.log(typeof "asd");
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+console.log("null == undefined", null == undefined);
+console.log("null === undefined", null === undefined);
+
+console.log("12 * null", 12 * null);
+console.log("12 * undefined", 12 * undefined);
+
+console.log("new Boolean(null)", new Boolean(null));
+console.log("new Boolean(undefined)", new Boolean(undefined));
