d99cbe80cc7eadcf3a179fa43fd093cb9c53acca
Ashish Saluja <ashishsaluja1@gmail.com>
2023-06-12 13:49:40 +0200

    Date data type

---
+++ DataTypes/DateDatatype.js
@@ -0,0 +1,6 @@
+const myDate = new Date();
+console.log('myDate', myDate);
+
+console.log('Date("2023-06-02")', new Date("2023-06-02"));
+console.log('Date("June 22, 2023 13:00:00")', new Date("June 22, 2023 13:00:00"));
+console.log('Date(2022, 1)', new Date(2022, 1));
