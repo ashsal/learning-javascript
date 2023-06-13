b02510b329a890a8ebc056f19a240f2d72f6b027
Ashish Saluja <ashishsaluja1@gmail.com>
2023-06-13 09:38:17 +0200

    Data data type examples

--- DataTypes/DateDatatype.js
+++ DataTypes/DateDatatype.js
@@ -1,6 +1,18 @@
 const myDate = new Date();
-console.log('myDate', myDate);
+console.log("myDate", myDate);
 
-console.log('Date("2023-06-02")', new Date("2023-06-02"));
-console.log('Date("June 22, 2023 13:00:00")', new Date("June 22, 2023 13:00:00"));
-console.log('Date(2022, 1)', new Date(2022, 1));
+console.log("myDate", new Date());
+console.log('new Date("2023-06-13T07:16:00.422Z")', new Date("2023-06-13T07:16:00.422Z"));
+console.log('new Date("2023-06-13")', new Date("2023-06-13"));
+console.log('new Date("ramesh-06-13")', new Date("ramesh-06-13"));
+console.log('new Date("ashish-ashish-13")', new Date("ashish-ashish-13"));
+
+console.log('new Date(2023, 1, 16, 14, 55, 30, 450)', new Date(2023, 1, 16, 14, 55, 30, 450));
+
+console.log('new Date(2023, 1, 16, 14, 55, 30)', new Date(2023, 1, 16, 14, 55, 30));
+console.log('new Date(2023, 1, 16, 14, 55)', new Date(2023, 1, 16, 14, 55));
+console.log('new Date(2023, 1, 16, 14)', new Date(2023, 1, 16, 14));
+console.log('new Date(2023, 1, 16)', new Date(2023, 1, 16));
+console.log('new Date(2023, 1)', new Date(2023, 1));
+console.log('new Date(600)', new Date(600));
+console.log('new Date(1100)', new Date(1100));
