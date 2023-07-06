5fd87b91a2a7c00a952c2304698c1d18254804f8
Ashish Saluja <ashishsaluja1@gmail.com>
2023-07-06 11:55:28 +0200

    Array Multi dimension

---
+++ DataTypes/ArrayMulti.js
@@ -0,0 +1,13 @@
+const numbers = [1, 3, 5, 7];
+console.log("numbers", numbers);
+console.log("numbers[0]", numbers[0]);
+console.log("numbers[2]", numbers[2]);
+
+const twoDArray = [[1,2,3],[4,5,6]];
+console.log("twoDArray", twoDArray);
+console.log("twoDArray[0][0]", twoDArray[0][0]);
+console.log("twoDArray[0][1]", twoDArray[0][1]);
+console.log("twoDArray[1][1]", twoDArray[1][1]);
+
+const classStudents = [["Salman", 25], ["Shahrukh", 26], ["Akshay", 27]];
+console.log("Shahrukh age", classStudents[1][1]);
