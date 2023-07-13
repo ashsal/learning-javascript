8563d2454195a2053b0ec38cbe7e928f997f1c93
Ashish Saluja <ashishsaluja1@gmail.com>
2023-07-13 12:52:46 +0200

    While loop examples

---
+++ WhileLoop.js
@@ -0,0 +1,61 @@
+let i = 1;
+while(i <= 10)
+{
+    //console.log(i);
+    i++;
+}
+
+let a = 10;
+while(a >= 1)
+{
+    //console.log(a);
+    a--;
+}
+
+let b = 1;
+while(b <= 10)
+{
+    //console.log(b);
+    b = b + 2;
+}
+
+const numbers = [12, 14, 16, 18, 19];
+let c = 0;
+while(c < numbers.length)
+{
+    //console.log(numbers[c]);
+    c++;
+}
+
+const myName = "Ashish Saluja";
+let d = 0;
+while(d < myName.length) {
+    //console.log(myName[d]);
+    d++;
+}
+
+let e = myName.length - 1;
+while(e >= 0) {
+    //console.log(myName[e]);
+    e--;
+}
+
+let f = 1;
+while(f <= 10) {
+    let g = 1;
+    while(g <= 10)
+    {   
+        const result = f * g;
+        console.log(`${f} * ${g} = ${result}`);
+        g++;
+    }
+    console.log("--------------");
+    f++;
+}
+
+let h = 1;
+while(true) {
+    console.log(h);
+    h++;
+}
+
