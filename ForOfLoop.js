76810c5b304b5b8109f22504af96d506bd07ed5f
Ashish Saluja <ashishsaluja1@gmail.com>
2023-07-15 07:58:24 +0200

    For of loop examples

---
+++ ForOfLoop.js
@@ -0,0 +1,15 @@
+const numbers = [123, 456, 345, 47, 87];
+
+for(let i = 0; i < numbers.length; i++)
+{
+    //console.log(numbers[i]);
+}
+
+for (const number of numbers) {
+    console.log(number);
+}
+
+const myName = "Ashish Saluja";
+for(const a of myName) {
+    console.log(a);
+}
