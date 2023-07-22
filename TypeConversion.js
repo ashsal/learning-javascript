d3881a2753cacca2ebaf9e7daa7dbcf496b4449e
Ashish Saluja <ashishsaluja1@gmail.com>
2023-07-22 09:22:11 +0200

    Type conversion example

---
+++ TypeConversion.js
@@ -0,0 +1,27 @@
+let num = 5; 
+let str = "Ashish";
+
+let sumResult = num + str; 
+console.log("sumResult", sumResult);
+
+let myBool = false; 
+let boolSum = myBool + 10;
+console.log("boolSum", boolSum);
+
+let strNumber = "56";
+let myNumber = Number(strNumber);
+console.log("myNumber", myNumber, strNumber);
+
+let mySecondNumber = -1;
+let booleanValue = Boolean(mySecondNumber);
+console.log("booleanValue", booleanValue);
+
+let myNumberThree = 723;
+let stringResult = String(myNumberThree);
+console.log("stringResult", stringResult, myNumberThree);
+
+let decimalString = "456.9";
+let floatResult = parseFloat(decimalString);
+console.log("floatResult", floatResult)
+
+
