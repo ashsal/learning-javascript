01ed59c94165143cfbafb969c5ffdc9b307df987
Ashish Saluja <ashishsaluja1@gmail.com>
2023-08-02 10:24:58 +0200

    Hoisting example

---
+++ Hoisting.js
@@ -0,0 +1,27 @@
+let a;
+a = 5;
+
+console.log(b);
+
+var b = 2;
+
+
+f1();
+function f1() {
+    console.log("Inside f1");
+}
+
+//console.log("c is", c);
+let c = 3;
+
+//f2();
+let f2 = function() {
+    console.log("Inside f2");
+}
+
+
+console.log(d);
+var d = 5;
+
+
+
