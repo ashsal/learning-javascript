8fd7223cef03c798c243882acac8aa2eb57c3baf
Ashish Saluja <ashishsaluja1@gmail.com>
2023-08-13 11:11:31 +0200

    Module introduction

---
+++ Modules/MathModule.js
@@ -0,0 +1,15 @@
+let result = 3;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+export { add, subtract, result, multiply }
---
+++ Modules/ModuleInitializer.js
@@ -0,0 +1,5 @@
+import { add, subtract, result, multiply } from './MathModule.js';
+
+console.log('Result of addition is', add(2, 3));
+console.log("Result of subtract is", subtract(3, 2));
+console.log("Value of result is", result);
